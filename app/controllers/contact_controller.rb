e826f54347a4b60b9c341c9683fb038314a050c6
Branden Mittra <branden.mittra@streamingwell.com>
2013-09-02 18:01:26 +0100

    Fix App Crash Test

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,20 +1,19 @@
 class ContactController < ApplicationController
 
-  def new
+def new
     @message = Message.new
   end
 
   def create
     @message = Message.new(params[:message])
     
-    respond_to do |format|
-      if @message.valid?
-        NotificationsMailer.new_message(@message).deliver
+    if @message.valid?
+      NotificationsMailer.new_message(@message).deliver
       redirect_to(live_path, :notice => "Message was successfully sent.")
-      else
-        flash.now.alert = "Please fill all fields."
-        render :new
-      end
+    else
+      flash.now.alert = "Please fill all fields."
+      render :new
     end
+  end
 
 end
