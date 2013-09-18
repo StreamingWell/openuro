9ac9479a586fe28cfe454c2ab8c69aa47a93c2d9
Branden Mittra <branden.mittra@streamingwell.com>
2013-09-18 11:54:21 +0100

    Try form button fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,15 +67,4 @@ Openmicrosite::Application.configure do
 
   # In production, :host should be set to the actual host of your application.
   config.action_mailer.default_url_options = { :host => 'http://warm-dawn-9465.herokuapp.com/' }
-
-  # Email configuration for SendGrid with Devise and Heroku
-  # config.action_mailer.default_url_options = { :host => 'http://warm-dawn-9465.herokuapp.com/' }
-  # ActionMailer::Base.smtp_settings = {
-  #  :address        => "smtp.sendgrid.net",
-  #  :port           => "25",
-  #  :authentication => :plain,
-  #  :user_name      => ENV['SENDGRID_USERNAME'],
-  #  :password       => ENV['SENDGRID_PASSWORD'],
-  #  :domain         => ENV['http://warm-dawn-9465.herokuapp.com/']
-  #}
 end
