6695298d5e4b95639b77d619900355a7a7682793
Edward Wallitt <edwallitt@me.com>
2013-10-16 17:41:17 +0100

    changes to copy in feedback controller and welcome email

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -7,8 +7,8 @@ class FeedbackController < ApplicationController
   def create
     @feedback = Feedback.new(params[:feedback])
     if @feedback.valid?
-      NotificationsMailer.new_feedback(@feedback, current_user).deliver
-      redirect_to(live_path, :notice => "Your feedback was successfully submitted.")
+      # NotificationsMailer.new_feedback(@feedback, current_user).deliver
+      redirect_to(root_path, :notice => "Your feedback was successfully submitted.")
     else
       redirect_to(evaluation_path, :notice => "Please fill all fields.")
     end
