be18a03df7895629c9268fb1ad1d7ccce175cc71
Edward Wallitt <edwallitt@me.com>
2013-10-16 17:47:21 +0100

    re-enable notifications and adjustments to html welcome email

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -7,7 +7,7 @@ class FeedbackController < ApplicationController
   def create
     @feedback = Feedback.new(params[:feedback])
     if @feedback.valid?
-      # NotificationsMailer.new_feedback(@feedback, current_user).deliver
+      NotificationsMailer.new_feedback(@feedback, current_user).deliver
       redirect_to(root_path, :notice => "Your feedback was successfully submitted.")
     else
       redirect_to(evaluation_path, :notice => "Please fill all fields.")
