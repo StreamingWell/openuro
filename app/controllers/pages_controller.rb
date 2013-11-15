907ec2b02793738d664f144b2e6b7a0939490015
Branden Mittra <branden.mittra@streamingwell.com>
2013-11-15 18:38:27 +0000

    Test push live

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -31,7 +31,7 @@ class PagesController < ApplicationController
     @feedback = Feedback.new
   end
 
-  #protected
+  protected
 
     #def authenticate
   	#  authenticate_or_request_with_http_basic do |username, password|
