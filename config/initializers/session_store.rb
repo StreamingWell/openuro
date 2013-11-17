1cf8b8426a60044e9b43de91a1cfb646dcce2e55
streamingwell <branden.mittra@streamingwell.com>
2013-11-17 21:52:45 +0000

    revert sessions_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Openmicrosite::Application.config.session_store :cookie_store, domain: :all, key: '_openmicrosite_session'
-
-# Allow log in on all devices
+Openmicrosite::Application.config.session_store :cookie_store, key: '_openmicrosite_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
