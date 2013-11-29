bf62327f635a48e02a39e2dce290b8556101e81a
Branden Mittra <branden.mittra@streamingwell.com>
2013-11-29 11:39:13 +0000

    Fix login issue within application layout and controller

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Openmicrosite::Application.config.session_store :cookie_store, key: '_openmicrosite_session', domain: :all
+Openmicrosite::Application.config.session_store :cookie_store, key: '_openmicrosite_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
