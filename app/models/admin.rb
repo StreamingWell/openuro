50fcc26c459d12a0de244d161e41ecf9a5ac30ad
Branden Mittra <branden.mittra@streamingwell.com>
2013-12-02 12:41:33 +0000

    Revert admin

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,4 +1,4 @@
-#class Admin < ActiveRecord::Base
-  #devise :database_authenticatable, :trackable, :timeoutable, :lockable  
- # attr_accessible :email, :password, :password_confirmation
-#end
+class Admin < ActiveRecord::Base
+  devise :database_authenticatable, :trackable, :timeoutable, :lockable  
+  attr_accessible :email, :password, :password_confirmation
+end
