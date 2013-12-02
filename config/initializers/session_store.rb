08a94ca6f522b360679233d290de6c6e2a5084a1
Branden Mittra <branden.mittra@streamingwell.com>
2013-12-02 13:42:19 +0000

    Test session store again

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Openmicrosite::Application.config.session_store :cookie_store, key: '_astellasoncology_session'
+Openmicrosite::Application.config.session_store :cookie_store, key: '_astellasoncology_session', domain: :all
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
